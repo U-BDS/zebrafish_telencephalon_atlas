ee701b2fd39025d1d0a5f1d7c2a138896ea9630a
Lara Ianov <lianov@uab.edu>
2022-12-21 18:16:55 -0600

    added a few more global vars

--- global.R
+++ global.R
@@ -1,4 +1,4 @@
-#-------------------------------packages-------------------------------------
+#-------------------------------packages------------------------------------------------------
 library(Seurat)
 library(cowplot)
 library(shiny)
@@ -8,10 +8,13 @@ library(markdown)
 library(shinyhelper)
 library(dplyr)
 
-#--------------------------custom functions----------------------------------
+#--------------------------custom functions---------------------------------------------------
 lapply(list.files("./R"), FUN = function(x) source(paste0("./R/", x)))
 
-#--------------------------global objects------------------------------------
+#--------------------------global objects/variables-----------------------------------------------------
+main_panel_style <- "overflow-y:scroll; max-height: 1250px; max-width: 1100px; border-top: solid; border-bottom: solid; border-color: #e8e8e8"
+
+#NOTE: due to simplicity just keeping simple/separate vectors, if needed will mapply all this
 # scRNA-seq datasets
 forebrain_integrated <- readRDS(file = "./data/forebrain_integrated_lean.rds")
 dpf6 <- readRDS(file = "./data/dpf6_lean.rds") 
@@ -24,3 +27,8 @@ colors_dpf6 <- as.character(read.csv("./data/custom_colors_dpf6.csv")$x)
 colors_df15 <- as.character(read.csv("./data/custom_colors_df15.csv")$x)
 colors_adult <- as.character(read.csv("./data/custom_colors_adult.csv")$x)
 
+# cluster names for each dataset
+cluster_names_forebrain_integrated <- levels(forebrain_integrated)
+cluster_names_dpf6 <- levels(dpf6)
+cluster_names_dpf15 <- levels(dpf15)
+cluster_names_adult <- levels(adult)
